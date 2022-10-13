eab75df20cddff33ee06123544756280fbc60e4e
dannybabu <danny1babu2@gmail.com>
2022-10-13 09:19:02 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *prints exactly "Programming is like building a multilingual puzzle, followed by a new line."
+ *and retuns always 0; 
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
