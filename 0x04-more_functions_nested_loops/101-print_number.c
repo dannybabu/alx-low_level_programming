69f0cbefefe6c7751d7a12521e3586b1630b8ae9
dannybabu <danny1babu2@gmail.com>
2022-10-20 14:02:40 -0700

    Task 12. Number have life

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n:.input integer parameter
+ * owned by zachee
+ */
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	putchar(i % 10 + '0');
+}
