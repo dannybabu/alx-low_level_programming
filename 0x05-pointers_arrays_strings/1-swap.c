d46740e47fb427ffecd77587f2703a58daf03193
dannybabu <danny1babu2@gmail.com>
2022-10-24 07:06:34 -0700

    Task 1 Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: An integer to swap
+ * @b: Another integer to swap
+ * owned by daniel
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int aux;
+
+	*a = *b;
+	*b = aux;
+}
