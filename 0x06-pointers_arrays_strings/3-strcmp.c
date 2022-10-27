bcf87b76e07742aeffb583bc684dcfca60ba3995
dannybabu <danny1babu2@gmail.com>
2022-10-27 16:12:42 -0700

    Task 12.A dream

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strcmp - Compares pointers to two strings.
  * @s1: A pointer to the first string to be compared.
@@ -8,12 +9,20 @@
  *         If s1 > s2, the positive difference of
  *         the first unmatched characters.
  */
+
 int _strcmp(char *s1, char *s2)
 {
-	while ((*s1 && *s2) && (*s1 == *s2))
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
 	{
 		s1++;
 		s2++;
 	}
-	return (*s1 - *s2);
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
