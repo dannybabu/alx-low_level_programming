69bd9352246c9cff4c1576ea35e5a688954378e2
dannybabu <danny1babu2@gmail.com>
2022-10-27 15:23:22 -0700

    Task 9.print integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,11 +10,11 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		n *= -1;
 	}
 	x = n;
 	if (x / 10)
 		print_number(x / 10);
-	_putchar(x % 10 + '0');
+	putchar(x % 10 + '0');
 }
