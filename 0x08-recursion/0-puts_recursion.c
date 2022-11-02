8a54eee380d0f4122874ea018540ddb706e636d8
dannybabu <danny1babu2@gmail.com>
2022-11-02 13:57:08 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,12 +10,12 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
+		putchar('\n');
 		return;
 	}
 
-	_putchar(*s);
+	putchar(*s);
 	s++;
-	_puts_recursion(s);
+	puts_recursion(s);
 }
 
