5835e1d2597513f2744f061a8cb21c8777be1e37
dannybabu <danny1babu2@gmail.com>
2022-11-10 16:26:23 -0800

    Add Task realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,7 +17,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	if ((new_size == 0) && (ptr != NULL))
 	{
-		ifree(ptr);
+		free(ptr);
 		return (NULL);
 	}
 	if (ptr == NULL)
@@ -34,7 +34,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			return (nptr);
 		for (i = 0; i < old_size; i++)
 			nptr[i] = *((char *)ptr + 1);
-		free (ptr);
+		free(ptr);
 	}
 
 	return (nptr);
