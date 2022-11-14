9fb78e6fb9f4c3238ec580d9c9b7aa417e21c044
dannybabu <danny1babu2@gmail.com>
2022-11-14 11:27:23 -0800

    Add Task dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,11 +2,13 @@
 #include "dog.h"
 
 /**
- * init_dog -initializes a variable of type struct dog.
- * @d: the dog to be initialized.
- * @name: the name of the dog.
- * @age: the age of the dog.
- * @owner: the ower of the dog.
+ * init_dog - Initializes a dog structure
+ * @d: A dog structure.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: Nothing
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,29 +1,30 @@
-#ifndef _DOG_H
-#define _DOG_H
+#ifndef _HEADER_
+#define _HEADER_
 
 /**
- * struct dog - Dog attributes
+ * struct dog - dog attributes
  * @name: The name of the dog.
  * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * @owner: The owner of the dog
  *
  * Description: The attributes of a dog.
  */
 
 struct dog
 {
-	char *name
-		float age;
+	char *name;
+	float age;
 	char *owner;
 };
+
 /**
  * dog_t - Typedef for dog structure
  */
-
 typedef struct dog dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /*DOG_*/
+#endif
