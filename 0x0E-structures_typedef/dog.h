647ce8d21acf7456eea6e9ba670df57c478d09fe
dannybabu <danny1babu2@gmail.com>
2022-11-14 09:56:29 -0800

    Add task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,12 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _HEADER_
+#define _HEADER_
 
 /**
  * struct dog - Dog attributes
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
+ *
  * Description: The attributes of a dog.
  */
 
@@ -15,14 +16,14 @@ struct dog
 		float age;
 	char *owner;
 };
-
 /**
  * dog_t - Typedef for dog structure
  */
 
 typedef struct dog dog_t;
+
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /*DOG_H*/
+#endif /*DOG_*/
